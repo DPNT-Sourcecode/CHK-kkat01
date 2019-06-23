56bf3882bf450afcab5f8cc257994f713c3a51d2
sbx_user1051 <sbx_user1051@169.254.64.29>
2019-06-23 14:29:34 +0000

    Sun Jun 23 14:29:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    if not isinstance(skus, unicode):
+    if not isinstance(skus, str):
         return -1
     print(skus)
     skus_list = skus.split(',')
+    print(skus_list)
     #raise NotImplementedError()
 
 checkout('C,D')
+
 
 
 
