a5485a9c13d1660cb6c2787219e5ec282b9b5964
sbx_user1051 <sbx_user1051@169.254.64.29>
2019-06-23 14:35:34 +0000

    Sun Jun 23 14:35:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,11 +6,15 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     print(skus)
-    skus_list = skus.split(',')
+    skus_list = split(skus)
+    for SKU in skus_list:
+        if SKU not in ['A','B','C','D']:
+            return -1
     print(skus_list)
     #raise NotImplementedError()
 
 checkout('C,D')
+
 
 
 
